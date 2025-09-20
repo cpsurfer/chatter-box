177ebc3262391cf09474c9fa127bcefc560f48dd
cpsurfer <firstrahul39@gmail.com>
2025-09-20 18:39:47 +0530

    k

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ app.use("/api/chat", protectRoute, chatRoutes);
 
 // Corrected Code
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
