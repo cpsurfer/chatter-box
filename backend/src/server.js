35e979f974932da83a3e5a93b1822f670c197a16
cpsurfer <firstrahul39@gmail.com>
2025-09-20 18:35:33 +0530

    k

--- backend/src/server.js
+++ backend/src/server.js
@@ -28,11 +28,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", protectRoute, userRoutes);
 app.use("/api/chat", protectRoute, chatRoutes);
 
-if(process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+// Corrected Code
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
 }
 
 app.listen(PORT, () => {
